cd903b27194d74babc11c07b06d78f29015cebab
jalvaradoWD <jalvarado1337@lavabit.com>
2020-10-18 16:10:00 -0500

    Functionality to add and remove features for the car.

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,11 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { add_price, subtract_price } from "../redux/types";
 
 const AdditionalFeature = (props) => {
+  const { name, price } = props.feature;
+  const dispatch = useDispatch();
+
+  const [selected, setSelected] = useState(false);
+
+  const buttonClick = () => {
+    switch (selected) {
+      case true:
+        dispatch({ type: add_price, payload: price });
+        break;
+      case false:
+        dispatch({ type: subtract_price, payload: price });
+        break;
+      default:
+        return null;
+    }
+
+    setSelected(!selected);
+  };
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
-      {props.feature.name} (+{props.feature.price})
+      <button onClick={buttonClick} className="button">
+        {!selected ? "Add" : "Remove"}
+      </button>
+      {name} (+{price})
     </li>
   );
 };
